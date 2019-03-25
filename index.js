558f93a4bff88560a4f03f218291eb92b591c874
MegaChuck64 <cscarey@email.neit.edu>
2019-03-24 20:38:29 -0400

    Locally saved color theme selection. Navbar with dropdown and links

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 
+var pickingColor = false;
+
 function Start()
 {
-    document.body.style.backgroundColor = localStorage.getItem("bColor");
-    document.getElementById("backgroundColor").value = localStorage.getItem("bColor");
+    //body
+    document.body.style.backgroundColor = localStorage.getItem("body_color");
+    document.getElementById("backgroundColor").value = localStorage.getItem("body_color");
+
+    //nav bar
+    document.getElementById("navbar").style.backgroundColor = localStorage.getItem("navbar_color");
+    document.getElementById("navbarColor").value = localStorage.getItem("navbar_color");
 }
 
-function ChangeColor(colorPicker)
+function PickingColor()
 {
-    document.body.style.backgroundColor = colorPicker.value;
-    localStorage.setItem("bColor", colorPicker.value);
+    pickingColor = true;
+}
+
+function ChangeColor(colorPicker, id)
+{
+    var el = document.getElementById(id);
+    el.style.backgroundColor = colorPicker.value;
+    localStorage.setItem(id + "_color", colorPicker.value);
+    pickingColor = false;
+}
+
+
+function DropdownClick(content)
+{
+    var el = document.getElementById(content);
+    el.classList.toggle("show");
+}
+
+
+window.onclick = function (event)
+{
+    if (!pickingColor)
+    {
+        if (!event.target.matches('.dropdownBtn'))
+        {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++)
+            {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show'))
+                {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
 }
